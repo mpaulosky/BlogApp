98253f5549368a98322f240ef5fa0c6eaed8ba52
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-04-22 15:37:19 -0700

    Add initial setup for BlogApp.AppHost application
    Introduce the core project structure with configurations, dependencies, and launch settings. This includes development and production appsettings, basic logging configuration, test project setup, and application entry point in Program.cs.

--- BlogApp.AppHost/Program.cs
+++ BlogApp.AppHost/Program.cs
@@ -1,11 +1,3 @@
-using Projects;
-
 var builder = DistributedApplication.CreateBuilder(args);
-
-var cache = builder.AddRedis("redis");
-
-// Add resource of BlogApp.Api
-var api = builder.AddProject<BlogApp_Api>("blog-app-api")
-		.WithReference(cache);
 
 builder.Build().Run();
